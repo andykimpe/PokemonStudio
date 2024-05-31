a18dfaefb79e95e9189f84ba1b5ed82d29357c18
Kimpe Andy <andykimpe@gmail.com>
2024-05-31 02:46:40 +0200

    Create pokemon-studio.sh

---
+++ linux-build/specfileforscl/pokemon-studio-scl/pokemon-studio.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#note echo 'makeself.sh --notemp ./backup ./backup.run "SFX installer for program" ./install.sh'
+source /opt/pokemon-studio/enable
+/opt/pokemon-studio/root/usr/bin/pokemon-studio $@
